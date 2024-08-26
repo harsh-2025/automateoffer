34ee048442fb62a07674438b0d701f68681da4e3
Harsh Rawat <110854500+harsh-2025@users.noreply.github.com>
2024-08-26 22:31:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+
+
 const express = require('express');
 const { Builder, By, until } = require('selenium-webdriver');
-const fs = require('fs');
 const chrome = require('selenium-webdriver/chrome');
 require('dotenv').config();
 
@@ -8,10 +9,14 @@ const app = express();
 const port = 4000;
 
 async function example() {
-  const config = JSON.parse(fs.readFileSync('data.json', 'utf8'));
   let chromeOptions = new chrome.Options();
-  chromeOptions.addArguments("--headless");
+  // chromeOptions.addArguments('--headless');
+  chromeOptions.addArguments('--no-sandbox');
   chromeOptions.addArguments('--disable-popup-blocking'); // To avoid any popup issues
+  chromeOptions.addArguments('--disable-dev-shm-usage');
+  chromeOptions.addArguments('--disable-gpu');
+  chromeOptions.addArguments('--remote-debugging-port=9222');
+  chromeOptions.addArguments('--verbose');
 
   let driver = await new Builder()
     .forBrowser('chrome')
@@ -29,22 +34,22 @@ async function example() {
     let emailInput = await driver.wait(until.elementLocated(By.xpath('//*[@id="Email or Mobile Number"]')), 30000000);
     await emailInput.sendKeys(process.env.email);
 
-    let emailSubmit = await driver.wait(until.elementLocated(By.xpath('//*[@id="react-root"]/div/div/div[3]/div[2]/div[1]/div[1]/form/div/div/div[3]/button')), 30000000);
+    let emailSubmit = await driver.wait(until.elementLocated(By.xpath('//*[@id="react-root"]/div/div/div[3]/div[2]/div[1]/div[1]/form/div/div/div[3]/button')), 3000000);
     await emailSubmit.click();
 
-    let passwordInput = await driver.wait(until.elementLocated(By.xpath('//*[@id="Password"]')), 30000000);
+    let passwordInput = await driver.wait(until.elementLocated(By.xpath('//*[@id="Password"]')), 3000000);
     await passwordInput.sendKeys(process.env.password);
 
-    let submitButton = await driver.wait(until.elementLocated(By.xpath('//*[@id="react-root"]/div/div/div[3]/div[2]/div[1]/div[1]/form/div/div/div[4]/button')), 30000000);
+    let submitButton = await driver.wait(until.elementLocated(By.xpath('//*[@id="react-root"]/div/div/div[3]/div[2]/div[1]/div[1]/form/div/div/div[4]/button')), 3000000);
     await submitButton.click();
 
-    let offerLink = await driver.wait(until.elementLocated(By.xpath('//*[@id="react-root"]/div/div[2]/div[1]/div[2]/nav/div/div[6]/a[1]')), 30000000);
+    let offerLink = await driver.wait(until.elementLocated(By.xpath('//*[@id="react-root"]/div/div[2]/div[1]/div[2]/nav/div/div[6]/a[1]')), 3000000);
     await offerLink.click();
 
-    let ofid = await driver.wait(until.elementsLocated(By.xpath('/html/body/div[1]/div/div[2]/div[1]/main/tabbed-container/content/div/div[2]/div[1]/table/tbody/tr[1]/td[1]/div/div/div/div/a/code')), 30000000);
+    let ofid = await driver.wait(until.elementsLocated(By.xpath('/html/body/div[1]/div/div[2]/div[1]/main/tabbed-container/content/div/div[2]/div[1]/table/tbody/tr[1]/td[1]/div/div/div/div/a/code')), 3000000);
     let text = await ofid[0].getText();
 
-    return text; // Return the text value
+    return text;
 
   } finally {
     console.log("Automation complete");
@@ -53,11 +58,10 @@ async function example() {
 }
 
 app.get('/start-automation', async (req, res) => {
-  // res.send('running fine');
   console.log('Automation started');
   
   try {
-    const text = await example();  // Call the example function and get the text
+    const text = await example();
     console.log('Text obtained:', text);
     res.send(`Automation completed! Text obtained: ${text}`);
   } catch (error) {
